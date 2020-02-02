94f3d8f1a9f14c93c35e1055b6b0a8c6f0ff5956
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-02-02 16:20:53 +0700

    Update perm desc

--- modules/admin-broadcast/migrate.php
+++ modules/admin-broadcast/migrate.php
@@ -4,7 +4,7 @@ return [
     'LibUserPerm\\Model\\UserPerm' => [
         'data' => [
             'name' => [
-                'manage_broadcast' => ['group'=>'Broadcast','about'=>'Allow user to create content broadcast'],
+                'manage_broadcast' => ['group'=>'Broadcast','about'=>'Allow user to manage broadcast content'],
                 'manage_broadcast_contact' => ['group'=>'Broadcast','about'=>'Allow user to manage broadcast contact'],
                 'manage_broadcast_contact_group' => ['group'=>'Broadcast','about'=>'Allow user to manage broadcast contact group']
             ]
